de5700a3b70a958d26fcc31aae4218eb1d4110fd
Salamat Baichoroev <salamat@Salamats-MacBook-Pro.local>
2023-11-20 21:48:19 +0600

    mongodb connected

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { config } from 'dotenv';
+import connect from './config/db.js'
 
-config();
 const app = express();
 
 app.use(express.json());
@@ -9,5 +9,11 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    try {
+        config();
+        connect();
+        console.log(`Server is running on http://localhost:${port}`);
+    } catch (err) {
+        console.log(`Error: ${err}`)
+    } 
 });
