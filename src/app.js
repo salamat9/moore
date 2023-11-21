12a282b179ab72e8f5c87c23680b4c9ab94b1fe5
Salamat Baichoroev <salamat@Salamats-MacBook-Pro.local>
2023-11-21 17:02:11 +0600

    cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { config } from 'dotenv';
 import connect from './config/db.js'
 import routes from './routes/index.js'
@@ -6,6 +7,7 @@ import routes from './routes/index.js'
 config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
